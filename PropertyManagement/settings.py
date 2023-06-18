26b98c4f405fe52bd6f7bcd88852fb71bcfa8398
Codertjay <codertjay@gmail.com>
2023-06-18 14:00:06 +0100

    added ui new update

--- PropertyManagement/settings.py
+++ PropertyManagement/settings.py
@@ -185,7 +185,8 @@ EMAIL_HOST = config('EMAIL_HOST', default="smtp.gmail.com")
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config("EMAIL_PORT")
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 # Send test mail and other bugs info
 ADMINS = [("Afenikhena Favour", ("dev.codertjay@gmail.com"))]
